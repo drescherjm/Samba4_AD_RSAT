473927986967bc94919e2bd64c6cef9cc087bdcf
John M. Drescher <drescherjm@gmail.com>
2023-04-10 17:54:56 -0400

    Add a method to allow creating users and groups for testing.

--- functions/F_10_PKG_01_samba_08_create_users_and_groups.sh
+++ functions/F_10_PKG_01_samba_08_create_users_and_groups.sh
@@ -22,7 +22,7 @@ for row in $(echo "${group_data}" | jq -r '.[] | @base64'); do
 
    # Generate a GID by looking for non-used GIDs in the range of 2000 to 10000
    local gid_generated=$(getent group | awk -F: '{uid[$3]=1}END{for(x=2000;x<=10000;x++)if(!uid[x]){print x;exit}}')
-   samba-tool group create ${group} --nis-domain=${samba_domain} --gid-number=${gid_generated}
+   samba-tool group create "${group}" --nis-domain=${samba_domain} --gid-number=${gid_generated}
 
 done
 
@@ -33,5 +33,16 @@ for row in $(echo "${user_data}" | jq -r '.[] | @base64'); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
-   echo $(_jq '.UserName'), $(_jq '.Password'), $(_jq '.Groups')
+   #echo $(_jq '.UserName'), $(_jq '.Password'), $(_jq '.Groups')
+   local  username=$(_jq '.UserName')
+   local  password=$(_jq '.Password')
+   local  groups=$(_jq '.Groups')
+   local  uid_generated=$(getent passwd | awk -F: '{uid[$3]=1}END{for(x=2000;x<=10000;x++)if(!uid[x]){print x;exit}}')
+
+   samba-tool user create "${username}" ${password} --nis-domain=${samba_domain} --uid-number=${uid_generated} --gid-number=100 --login-shell=/bin/bash --unix-home=/home/${username}
+
+   for group in $(echo "${groups}" | jq -r '.[]' ); do
+     samba-tool group addmembers "${group}" "${username}"
+   done 
+
 done
