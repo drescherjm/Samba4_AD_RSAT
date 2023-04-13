ae24d42eb698b6c7b1d8784a3875d9466438484a
John M. Drescher <drescherjm@gmail.com>
2023-04-13 22:11:35 +0000

    Begin to add a mode feature to allow for creating the first DC or adding additional DCs to the domain

---
+++ functions/F_10_PKG_01_samba_00_set_mode.sh
@@ -0,0 +1,7 @@
+# =====================
+# Enable databag
+# =====================
+# DATABAG_CFG:enable
+
+# In this script I will introduce a mode option that will allow the user to set the mode of install.
+
